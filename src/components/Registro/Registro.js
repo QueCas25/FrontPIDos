d02273a6ec35302ae26d65b227e56bbe02c420d0
QueCas25 <jjquecas@outlook.com>
2023-11-09 23:15:24 -0600

    CSS de registro y algunas insaneadas

--- src/components/Registro/Registro.js
+++ src/components/Registro/Registro.js
@@ -5,6 +5,7 @@ import { MDBBtn, MDBContainer, MDBCard, MDBCardBody } from 'mdb-react-ui-kit';
 import Registro from '../../functions/Registro';
 import {registroSchema} from "../../functions/Validation/validationSchema";
 import CustomInput from "../../functions/Validation/customInput";
+import './styles.css';
 function Registrar() {
     const navigate = useNavigate();
 
@@ -30,7 +31,7 @@ function Registrar() {
             <MDBCard className="bg-cv">
                 <MDBCardBody className="d-flex flex-column">
                     <h3 className="fw-bold my-4 pb-3" style={{ letterSpacing: '1px' }}>
-                       Registro de Usuario
+                       Crea una cuenta
                     </h3>
                     <Formik
                         initialValues={{ name:"", email:"", password:"",confirmPassword:""}}
@@ -39,38 +40,51 @@ function Registrar() {
                     >
                         {({ isSubmitting }) => (
                             <Form>
-                                <div style={{ display: 'flex', alignItems: 'center', padding: '10px' }}>
-                                    <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
-                                            <div style={{ display: 'flex', alignItems: 'center' }}>
+                                    <div className="form-row">            
                                     <CustomInput
+                                        className="input-field"
                                         label="Nombre"
                                         name="name"
                                         type="input"
                                         placeholder="Juan Perez"
                                     />
+                                    <div/>
+                                    <div className="form-row">  
                                     <CustomInput
+                                    className="input-field"
                                         label="Correo"
                                         name="email"
                                         type="input"
                                         placeholder="E-mail"
                                     />
+                                    <div/>
+                                    <div className="form-row">  
                                     <CustomInput
+                                    className="input-field"
                                         label="Contraseña"
                                         name="password"
                                         type="input"
                                         placeholder="*****"
                                     />
+                                    <div/>
+                                    <div className="form-row">  
                                     <CustomInput
+                                    className="input-field"
                                         label="Confirme Contraseña"
                                         name="confirmPassword"
                                         type="input"
                                         placeholder="*****"
                                     />
+                                    <div/>
                                 <div className='text-center'>
-                                    <MDBBtn className="mb-4 px-5" color="dark" size="lg" disabled={isSubmitting} type="submit">Registrar</MDBBtn>
+                                    <MDBBtn className="mb-4 px-5" disabled={isSubmitting} type="submit">Registrar</MDBBtn>
                                 </div>
                                         </div>
                                     </div>
+                                </div>
+                                <div className='textoCuenta'>
+                                <a href='Login'>¿Ya tienes una cuenta? Da click aquí para iniciar sesión.</a>
+                                </div>
                                 </div>
                             </Form>
                         )}
